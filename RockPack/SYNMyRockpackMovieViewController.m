adc4c148919150df0eaff0cbbfcba513f5c72329
Nick Banks <nick.banks@synchromation.com>
2012-11-29 17:46:42 +0000

    Fixed strange black-screen video bug

--- RockPack/SYNMyRockpackMovieViewController.m
+++ RockPack/SYNMyRockpackMovieViewController.m
@@ -52,19 +52,29 @@
 //    SYNAppDelegate *delegate = (SYNAppDelegate *)[[UIApplication sharedApplication] delegate];
     [[self.mainVideoPlayer view] setFrame: CGRectMake (0, 0, 1024, 642)]; // Frame must match parent view
     
-    [self.view insertSubview: [self.mainVideoPlayer view] atIndex: 0];
+    [self.view insertSubview: self.mainVideoPlayer.view atIndex: 0];
         
     [self.mainVideoPlayer pause];
 }
 
+// Don't call these here as called when going full-screen
+
 - (void) viewWillDisappear: (BOOL) animated
 {
+    // Don't call these here as called when going full-screen
     // Remember to kill any video that might be active
-    [self.mainVideoPlayer pause];
-    self.mainVideoPlayer = nil;
+//        self.mainVideoPlayer.view.hidden = TRUE;
+//    [self.mainVideoPlayer pause];
+//
+//    self.mainVideoPlayer.view.hidden = TRUE;
+    [self.mainVideoPlayer.view removeFromSuperview];
+    
+//    self.mainVideoPlayer = nil;
     
     [super viewWillDisappear: animated];
 }
+
+
 
 - (IBAction) popCurrentView: (id) sender
 {
