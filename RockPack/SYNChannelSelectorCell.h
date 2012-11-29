4f5269cd52a2bc8073c10c65a91c1b3c54326ee4
Nick Banks <nick.banks@synchromation.com>
2012-11-29 09:28:12 +0000

    Adding new coverflow picker

---
+++ RockPack/SYNChannelSelectorCell.h
@@ -0,0 +1,13 @@
+//
+//  SYNChannelSelectorCell.h
+//  rockpack
+//
+//  Created by Nick Banks on 28/11/2012.
+//  Copyright (c) 2012 Nick Banks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYNChannelSelectorCell : UICollectionViewCell
+
+@end
---
+++ RockPack/SYNChannelSelectorCell.m
@@ -0,0 +1,31 @@
+//
+//  SYNChannelSelectorCell.m
+//  rockpack
+//
+//  Created by Nick Banks on 28/11/2012.
+//  Copyright (c) 2012 Nick Banks. All rights reserved.
+//
+
+#import "SYNChannelSelectorCell.h"
+
+@implementation SYNChannelSelectorCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
