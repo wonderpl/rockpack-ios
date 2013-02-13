a59669584c5cb659e2f784903ffdab824d2212e0
Michael Michailidis <michaelmichailidis@Michaels-MacBook-Pro.local>
2013-02-13 17:16:25 +0000

    Last

--- RockPack/SYNAbstractTopTabViewController.m
+++ RockPack/SYNAbstractTopTabViewController.m
@@ -128,7 +128,7 @@
 
 -(void)handleSecondaryTap:(UITapGestureRecognizer *)recogniser
 {
-    SYNAppDelegate *appDelegate = UIApplication.sharedApplication.delegate;
+    //SYNAppDelegate *appDelegate = UIApplication.sharedApplication.delegate;
 }
 
 
