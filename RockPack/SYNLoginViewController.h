9b80a4db786a30fe602a1eb2301ceb12535facb3
Michael Michailidis <mmick66@gmail.com>
2013-03-11 09:53:51 +0000

    Created basic View Controller for Login Screen together with XIB file

---
+++ RockPack/SYNLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  SYNLoginViewController.h
+//  rockpack
+//
+//  Created by Michael Michailidis on 11/03/2013.
+//  Copyright (c) 2013 Nick Banks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYNLoginViewController : UIViewController
+
+@end
