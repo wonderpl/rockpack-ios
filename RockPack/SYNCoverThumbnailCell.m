e849dc231a5e573470f8c039aa64ae98dfd41189
Nick Banks <nick.banks@synchromation.com>
2013-04-25 12:59:29 +0100

    Added cover / category support

--- RockPack/SYNCoverThumbnailCell.m
+++ RockPack/SYNCoverThumbnailCell.m
@@ -7,25 +7,75 @@
 //
 
 #import "SYNCoverThumbnailCell.h"
+#import "UIImageView+ImageProcessing.h"
+#import "AppConstants.h"
+
+@interface SYNCoverThumbnailCell ()
+
+@property (nonatomic, assign)  BOOL coverSelected;
+@property (nonatomic, strong) IBOutlet UIImageView *selectedOverlayImageView;
+
+@end
+
 
 @implementation SYNCoverThumbnailCell
 
-- (id)initWithFrame:(CGRect)frame
+@synthesize coverSelected = _displayMode;
+
+- (void) awakeFromNib
 {
-    self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
-    }
-    return self;
+    [super awakeFromNib];
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
+
+#pragma mark - Selection
+
+- (void) setSelected: (BOOL) selected
 {
-    // Drawing code
+    [super setSelected: selected];
+    
+    if (selected)
+    {
+        // Guard against multiple repeated selections
+        if (self.selectedOverlayImageView.alpha == 0.0f)
+        {
+            [UIView animateWithDuration: kChannelEditModeAnimationDuration
+                             animations: ^{
+                                 self.selectedOverlayImageView.alpha = 1.0f;
+                             }
+                             completion: nil];
+        }
+    }
+    else
+    {
+        if (self.selectedOverlayImageView.alpha == 1.0f)
+        {
+            [UIView animateWithDuration: kChannelEditModeAnimationDuration
+                             animations: ^{
+                                 self.selectedOverlayImageView.alpha = 0.0f;
+                             }
+                             completion: nil];
+        }
+    }
 }
-*/
+
+
+#pragma mark - Asynchronous image loading support
+
+- (void) setCoverImageWithURLString: (NSString*) imageURLString
+{
+    [self.coverImageView setAsynchronousImageFromURL: [NSURL URLWithString: imageURLString]
+                                    placeHolderImage: nil];
+}
+
+
+
+// If this cell is going to be re-used, then clear the image and cancel any outstanding operations
+- (void) prepareForReuse
+{
+    // We need to clean up any asynchronous image uploads
+    self.coverImageView.image = nil;
+    self.selectedOverlayImageView.alpha = 0.0f;
+}
 
 @end
