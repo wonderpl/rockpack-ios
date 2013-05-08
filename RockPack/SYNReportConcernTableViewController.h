0d07fab4e4d728f9ce4293674a94796b987a99f7
Nick Banks <nick.banks@synchromation.com>
2013-05-08 23:41:36 +0100

    Added concerns table view controller

---
+++ RockPack/SYNReportConcernTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SYNReportConcernTableViewController.h
+//  rockpack
+//
+//  Created by Nick Banks on 08/05/2013.
+//  Copyright (c) 2013 Nick Banks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYNReportConcernTableViewController : UITableViewController
+
+@end
---
+++ RockPack/SYNReportConcernTableViewController.m
@@ -0,0 +1,83 @@
+//
+//  SYNReportConcernTableViewController.m
+//  rockpack
+//
+//  Created by Nick Banks on 08/05/2013.
+//  Copyright (c) 2013 Nick Banks. All rights reserved.
+//
+
+#import "SYNReportConcernTableViewController.h"
+
+#define kConcernsCellId @"ConcernsCell"
+
+@interface SYNReportConcernTableViewController ()
+
+@property (nonatomic, strong) NSArray *concernsArray;
+
+@end
+
+@implementation SYNReportConcernTableViewController
+
+- (id) initWithStyle: (UITableViewStyle) style
+{
+    if ((self = [super initWithStyle: style]))
+    {
+        // Custom initialization
+    }
+    
+    return self;
+}
+
+- (void) viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self.tableView registerClass: [UITableViewCell class]
+           forCellReuseIdentifier: kConcernsCellId];
+
+    self.concernsArray = @[@"Nudity or pornography",
+                           @"Attacks a group or individual",
+                           @"Graphic violence",
+                           @"Hateful speech or symbols",
+                           @"Actively promotes self-harm",
+                           @"Spam",
+                           @"Other"];
+}
+
+
+#pragma mark - Table view data source
+
+- (NSInteger) numberOfSectionsInTableView: (UITableView *) tableView
+{
+    return 1;
+}
+
+
+- (NSInteger) tableView: (UITableView *) tableView
+  numberOfRowsInSection: (NSInteger) section
+{
+    return self.concernsArray.count;
+}
+
+
+- (UITableViewCell *) tableView: (UITableView *) tableView
+          cellForRowAtIndexPath: (NSIndexPath *) indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: kConcernsCellId
+                                                            forIndexPath: indexPath];
+    cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    cell.textLabel.text = self.concernsArray[indexPath.row];
+    
+    return cell;
+}
+
+
+#pragma mark - Table view delegate
+
+- (void) tableView: (UITableView *) tableView
+         didSelectRowAtIndexPath: (NSIndexPath *) indexPath
+{
+
+}
+
+@end
