618eb222b92603376f6c07de8bee9e566a1a4646
Michael Michailidis <mmick66@gmail.com>
2013-05-09 10:53:42 +0100

    Changed Model Entity name from Category to Genre

---
+++ RockPack/SubGenre.h
@@ -0,0 +1,7 @@
+#import "_SubGenre.h"
+
+@interface SubGenre : _SubGenre {}
+
++ (SubGenre *)instanceFromDictionary: (NSDictionary *) dictionary usingManagedObjectContext: (NSManagedObjectContext *) managedObjectContext;
+
+@end
