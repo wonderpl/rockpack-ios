77e40f57178a3334094cf2a66e384de462faadc4
Nick Banks <nick.banks@synchromation.com>
2013-05-21 11:34:28 +0100

    Added age category for user's age in account settings

--- RockPack/SYNAccountSettingsMainTableViewController.m
+++ RockPack/SYNAccountSettingsMainTableViewController.m
@@ -9,6 +9,7 @@
 
 #import "AppConstants.h"
 #import "GAI.h"
+#import "NSString+Utils.h"
 #import "SYNAccountSettingTableViewCell.h"
 #import "SYNAccountSettingsAbout.h"
 #import "SYNAccountSettingsDOB.h"
@@ -27,6 +28,7 @@
 #import "UIFont+SYNFont.h"
 #import "User.h"
 
+
 @interface SYNAccountSettingsMainTableViewController ()
 
 @property (nonatomic, strong) NSArray* dataItems2ndSection;
@@ -37,15 +39,15 @@
 
 @end
 
+
 @implementation SYNAccountSettingsMainTableViewController
 
 @synthesize dataItems2ndSection, appDelegate, user;
 
-- (id)init
+- (id) init
 {
-    self = [super initWithStyle:UITableViewStyleGrouped];
-    if (self) {
-        
+    if ((self = [super initWithStyle: UITableViewStyleGrouped]))
+    {
         dataItems2ndSection = @[NSLocalizedString (@"Change Password", nil),
                                 NSLocalizedString (@"About", nil),
                                 NSLocalizedString (@"Logout", nil)];
@@ -55,12 +57,13 @@
         user = appDelegate.currentUser;
         
         self.title = @"ACCOUNT SETTINGS";
-        
     }
+    
     return self;
 }
 
-- (void)viewDidLoad
+
+- (void) viewDidLoad
 {
     [super viewDidLoad];
     
@@ -68,28 +71,28 @@
 
     self.tableView.scrollEnabled = [[SYNDeviceManager sharedInstance] isIPhone];
     
-    
-    UILabel* titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0, 10.0, 100.0, 20.0)];
+    UILabel* titleLabel = [[UILabel alloc] initWithFrame: CGRectMake(0.0, 10.0, 100.0, 20.0)];
     titleLabel.backgroundColor = [UIColor clearColor];
-    titleLabel.textColor = [UIColor colorWithRed:(28.0/255.0) green:(31.0/255.0) blue:(33.0/255.0) alpha:(1.0)];
+    titleLabel.textColor = [UIColor colorWithRed: (28.0/255.0) green: (31.0/255.0) blue: (33.0/255.0) alpha: (1.0)];
     titleLabel.text = @"Account Settings";
     titleLabel.font = [UIFont rockpackFontOfSize:16.0];
     
-    
     self.navigationItem.titleView = titleLabel;
-    
 }
 
-- (void) forcePopoverSize {
+
+- (void) forcePopoverSize
+{
     CGSize currentSetSizeForPopover = self.contentSizeForViewInPopover;
     CGSize fakeMomentarySize = CGSizeMake(currentSetSizeForPopover.width - 1.0f, currentSetSizeForPopover.height - 1.0f);
     self.contentSizeForViewInPopover = fakeMomentarySize;
     self.contentSizeForViewInPopover = currentSetSizeForPopover;
 }
 
--(void)viewWillAppear:(BOOL)animated
+
+- (void) viewWillAppear: (BOOL) animated
 {
-    [super viewWillAppear:animated];
+    [super viewWillAppear: animated];
     [self.tableView reloadData];
     
     self.tableView.backgroundView = nil;
@@ -98,49 +101,60 @@
     
 }
 
--(void)viewDidAppear:(BOOL)animated
+
+- (void) viewDidAppear: (BOOL) animated
 {
-    [super viewDidAppear:animated];
+    [super viewDidAppear: animated];
     
     
 }
+
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+- (NSInteger) numberOfSectionsInTableView: (UITableView *) tableView
 {
-    
     return 2;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+
+- (NSInteger) tableView: (UITableView *) tableView
+  numberOfRowsInSection: (NSInteger) section
 {
-    if(section == 0) { // first section
+    if (section == 0)
+    {
+        // first section
         return 6;
-    } else { // second section
+    }
+    else
+    {
+        // second section
         return dataItems2ndSection.count;
     }
     
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+
+- (UITableViewCell *) tableView: (UITableView *) tableView
+          cellForRowAtIndexPath: (NSIndexPath *) indexPath
 {
     UITableViewCell *cell;
     
-    
-    if(indexPath.section == 0) {
-        
+    if (indexPath.section == 0)
+    {
         static NSString *CellIdentifier = @"Section0Cell";
-        cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+        cell = [tableView dequeueReusableCellWithIdentifier: CellIdentifier];
+        
         if(!cell)
         {
-            cell = [[SYNAccountSettingTableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+            cell = [[SYNAccountSettingTableViewCell alloc] initWithStyle: UITableViewCellStyleSubtitle
+                                                         reuseIdentifier: CellIdentifier];
         }
         
-        switch (indexPath.row) {
-                
+        switch (indexPath.row)
+        {
             case 0:
-                cell.imageView.image = [UIImage imageNamed:@"IconFullname.png"];
+                cell.imageView.image = [UIImage imageNamed: @"IconFullname.png"];
                 cell.textLabel.text = user.firstName;
                 cell.detailTextLabel.text = user.fullNameIsPublicValue ? NSLocalizedString (@"Public" , nil) : NSLocalizedString (@"Private" , nil);
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
@@ -148,144 +162,157 @@
                 
                 
             case 1:
-                cell.imageView.image = [UIImage imageNamed:@"IconUsername.png"];
+                cell.imageView.image = [UIImage imageNamed: @"IconUsername.png"];
                 cell.textLabel.text = user.username;
                 cell.detailTextLabel.text = NSLocalizedString (@"Public", nil);
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
                 break;
                 
             case 2:
-                if([user.emailAddress isEqualToString:@""]) {
+                if ([user.emailAddress isEqualToString:@""])
+                {
                     cell.textLabel.text = @"Email Address";
-                } else {
+                }
+                else
+                {
                     cell.textLabel.text = user.emailAddress;
                 }
-                cell.imageView.image = [UIImage imageNamed:@"IconEmail.png"];
+                cell.imageView.image = [UIImage imageNamed: @"IconEmail.png"];
                 cell.detailTextLabel.text = NSLocalizedString (@"Email - Private", nil);
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
                 break;
                 
             case 3:
-                if([user.locale isEqualToString:@"en-gb"]) {
+                if ([user.locale isEqualToString:@"en-gb"])
+                {
                     cell.textLabel.text = NSLocalizedString (@"United Kingdom", nil);
-                } else {
+                }
+                else
+                {
                     cell.textLabel.text = NSLocalizedString (@"United States", nil);
                 }
                 
                 cell.detailTextLabel.text = @"Location";
-                cell.imageView.image = [UIImage imageNamed:@"IconLocation.png"];
+                cell.imageView.image = [UIImage imageNamed: @"IconLocation.png"];
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
                 break;
                 
             case 4:
-                if([user.gender isEqual:@(GenderUndecided)]) {
+                if ([user.gender isEqual:@(GenderUndecided)])
+                {
                     cell.textLabel.text = NSLocalizedString (@"Gender", nil);
-                } else {
-                    cell.textLabel.text = [user.gender isEqual:@(GenderMale)] ? @"Male" : @"Female";
+                }
+                else
+                {
+                    cell.textLabel.text = [user.gender isEqual: @(GenderMale)] ? @"Male" : @"Female";
                 }
                 
                 cell.detailTextLabel.text = NSLocalizedString (@"Gender - Private", nil);
-                cell.imageView.image = [UIImage imageNamed:@"IconGender.png"];
+                cell.imageView.image = [UIImage imageNamed :@"IconGender.png"];
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
                 break;
                 
             case 5:
-                if(!user.dateOfBirth)
+                if (!user.dateOfBirth)
                     cell.textLabel.text = NSLocalizedString (@"Date of Birth", nil);
                 else
                     cell.textLabel.text = [self getDOBPlainString:user.dateOfBirth];
                 
                 self.dobTableViewCell = cell;
                 cell.detailTextLabel.text = NSLocalizedString (@"D.O.B Private", nil);
-                cell.imageView.image = [UIImage imageNamed:@"IconBirthday.png"];
+                cell.imageView.image = [UIImage imageNamed: @"IconBirthday.png"];
                 break;
                 
         }
         
-        cell.textLabel.font = [UIFont rockpackFontOfSize:18.0];
-        cell.detailTextLabel.font = [UIFont rockpackFontOfSize:13.0];
+        cell.textLabel.font = [UIFont rockpackFontOfSize: 18.0];
+        cell.detailTextLabel.font = [UIFont rockpackFontOfSize: 13.0];
         cell.textLabel.backgroundColor = [UIColor clearColor];
-        
-        
-    } else {
-        
+    }
+    else
+    {
         static NSString *CellIdentifier = @"OtherSectionCell";
-        cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-        if(!cell)
-        {
-            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
-        }
+        cell = [tableView dequeueReusableCellWithIdentifier: CellIdentifier];
         
+        if (!cell)
+        {
+            cell = [[UITableViewCell alloc] initWithStyle: UITableViewCellStyleDefault
+                                          reuseIdentifier: CellIdentifier];
+        }
         
         cell.textLabel.text = (NSString*)dataItems2ndSection[indexPath.row];
         
         cell.textLabel.font = [UIFont rockpackFontOfSize:18.0];
         
-        if(indexPath.row != 2) {
+        if (indexPath.row != 2)
+        {
             cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
         }
-        
     }
     
     return cell;
 }
 
 
-
 #pragma mark - Table view delegate
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+- (void) tableView: (UITableView *) tableView
+         didSelectRowAtIndexPath: (NSIndexPath *) indexPath
 {
-    if(indexPath.section == 0)
+    if (indexPath.section == 0)
     {
-        
-        switch (indexPath.row) {
+        switch (indexPath.row)
+        {
                 
             case 0:
-                [self.navigationController pushViewController:[[SYNAccountSettingsFullNameInput alloc] initWithUserFieldType:UserFieldTypeFullName] animated:YES];
+                [self.navigationController pushViewController: [[SYNAccountSettingsFullNameInput alloc] initWithUserFieldType:UserFieldTypeFullName]
+                                                     animated: YES];
                 break;
                 
-            
-                
             case 1:
-                [self.navigationController pushViewController:[[SYNAccountSettingsUsername alloc] initWithUserFieldType:UserFieldTypeUsername] animated:YES];
+                [self.navigationController pushViewController: [[SYNAccountSettingsUsername alloc] initWithUserFieldType:UserFieldTypeUsername]
+                                                     animated: YES];
                 
                 break;
                 
             case 2:
-                [self.navigationController pushViewController:[[SYNAccountSettingsEmail alloc] initWithUserFieldType:UserFieldTypeEmail] animated:YES];
+                [self.navigationController pushViewController: [[SYNAccountSettingsEmail alloc] initWithUserFieldType:UserFieldTypeEmail]
+                                                     animated: YES];
                 break;
                 
             case 3:
-                [self.navigationController pushViewController:[[SYNAccountSettingsLocation alloc] init] animated:YES];
+                [self.navigationController pushViewController: [[SYNAccountSettingsLocation alloc] init]
+                                                     animated: YES];
                 break;
             
             case 4:
-                [self.navigationController pushViewController:[[SYNAccountSettingsGender alloc] init] animated:YES];
+                [self.navigationController pushViewController: [[SYNAccountSettingsGender alloc] init]
+                                                     animated: YES];
                 break;
                 
             case 5:
             {
                 SYNAccountSettingsDOB* dobController = [[SYNAccountSettingsDOB alloc] init];
                 
-                [dobController.datePicker addTarget:self
-                                             action:@selector(datePickerValueChanged:)
-                                   forControlEvents:UIControlEventValueChanged];
+                [dobController.datePicker addTarget: self
+                                             action: @selector(datePickerValueChanged:)
+                                   forControlEvents: UIControlEventValueChanged];
+                
                 NSDate* date = [NSDate date];
-                if(appDelegate.currentUser.dateOfBirth)
+                
+                if (appDelegate.currentUser.dateOfBirth)
                 {
                     date = appDelegate.currentUser.dateOfBirth;
                 }
+                
                 [dobController.datePicker setDate:date];
                 
-                if([[SYNDeviceManager sharedInstance] isIPad])
+                if ([[SYNDeviceManager sharedInstance] isIPad])
                 {
                     if(self.dobPopover)
                         return;
-                    
-                    
-                    
-                    UINavigationController* navigationController = [[UINavigationController alloc] initWithRootViewController:dobController];
+
+                    UINavigationController* navigationController = [[UINavigationController alloc] initWithRootViewController: dobController];
                     
                     
                     self.dobPopover = [[UIPopoverController alloc] initWithContentViewController: navigationController];
@@ -300,45 +327,42 @@
                 }
                 else
                 {
-                    
-                    
-                    [self.navigationController pushViewController:dobController animated:YES];
-                
+                    [self.navigationController pushViewController: dobController
+                                                         animated: YES];
                 }
-                
             }
             break;
                 
             default:
-                break;
-                
+                break;  
         }
-        
-    } else {
-        
-        switch (indexPath.row) {
-                
+    }
+    else
+    {
+        switch (indexPath.row)
+        { 
             case 0:
-                [self.navigationController pushViewController:[[SYNAccountSettingsPassword alloc] init] animated:YES];
+                [self.navigationController pushViewController: [[SYNAccountSettingsPassword alloc] init]
+                                                     animated: YES];
                 break;
                 
             case 1:
-                [self.navigationController pushViewController:[[SYNAccountSettingsAbout alloc] init] animated:YES];
+                [self.navigationController pushViewController: [[SYNAccountSettingsAbout alloc] init]
+                                                     animated: YES];
                 break;
                 
             case 2:
                 [self showLogoutAlert];
                 break;
-               
-                
         }
-    
     }
     
-    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
-
+    [self.tableView deselectRowAtIndexPath: indexPath
+                                  animated: YES];
 }
--(void)showLogoutAlert
+
+
+- (void) showLogoutAlert
 {
     UIAlertView *alert = [[UIAlertView alloc] initWithTitle: NSLocalizedString (@"Logout", nil)
                                                     message: NSLocalizedString (@"Are you sure you want to log out?", nil)
@@ -347,6 +371,7 @@
                                           otherButtonTitles: NSLocalizedString (@"Logout", nil), nil];
     [alert show];
 }
+
 
 - (void) alertView: (UIAlertView *) alertView
          clickedButtonAtIndex: (NSInteger) buttonIndex
@@ -362,12 +387,8 @@
 }
 
 
-
-
--(void)datePickerValueChanged:(UIDatePicker*)datePicker
+- (void) datePickerValueChanged: (UIDatePicker*) datePicker
 {
-    
-    
     NSString* dateString = [self getDOBFormattedString:datePicker.date];
     
     UIActivityIndicatorView* dobLoader = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
@@ -376,24 +397,17 @@
     
     [dobLoader startAnimating];
     
-    [self.appDelegate.oAuthNetworkEngine changeUserField:@"date_of_birth"
-                                                 forUser:self.appDelegate.currentUser
-                                            withNewValue:dateString
-                                       completionHandler:^ {
-                                           
+    [self.appDelegate.oAuthNetworkEngine changeUserField: @"date_of_birth"
+                                                 forUser: self.appDelegate.currentUser
+                                            withNewValue: dateString
+                                       completionHandler: ^ {
                                            user.dateOfBirth = datePicker.date;
-                                           
                                            self.dobTableViewCell.textLabel.text = [self getDOBPlainString:user.dateOfBirth];
-                                           
                                            [dobLoader stopAnimating];
-                                           
                                            [dobLoader removeFromSuperview];
-                                           
-                                       } errorHandler:^(id errorInfo) {
-                                           
-                                           
-                                           
-                                       }];
+                                       }
+                                            errorHandler:^(id errorInfo) {
+                                            }];
 
     // Calculate age, taking account of leap-years etc. (probably too accurate!)
     NSDateComponents* ageComponents = [[NSCalendar currentCalendar] components: NSYearCalendarUnit
@@ -402,7 +416,8 @@
                                                                        options: 0];
     
     NSInteger age = [ageComponents year];
-    NSString *ageString = [NSString stringWithFormat: @"%d", age];
+    
+    NSString *ageString = [NSString ageCategoryStringFromInt: age];
     
     // Now set the age
     id<GAITracker> tracker = [GAI sharedInstance].defaultTracker;
