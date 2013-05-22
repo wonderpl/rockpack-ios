436922b2092d0b44cdf166cb6606503d81b58ff6
Nick Banks <nick.banks@synchromation.com>
2013-05-22 16:42:54 +0100

    Added horizontal cover chooser 'More' supplementary view

---
+++ RockPack/SYNCoverRightMoreView.h
@@ -0,0 +1,13 @@
+//
+//  SYNCoverRightMoreView.h
+//  rockpack
+//
+//  Created by Nick Banks on 22/05/2013.
+//  Copyright (c) 2013 Nick Banks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYNCoverRightMoreView : UICollectionReusableView
+
+@end
