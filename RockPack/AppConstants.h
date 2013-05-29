fd893b22afa8aaa0487e4a0b9bcb9f55ee713875
Nick Banks <nick.banks@synchromation.com>
2013-05-29 11:59:00 +0100

    Updated iPhone slider positioning and displayed airplay icon

--- RockPack/AppConstants.h
+++ RockPack/AppConstants.h
@@ -340,9 +340,9 @@ typedef enum {
 
 #define kShuttleBarHeight               44.0f
 #define kShuttleBarTimeLabelWidth       40.0f
-#define kShuttleBarTimeLabelOffset      100.0f
-#define kShuttleBarButtonWidth          77.0f
-#define kShuttleBarSliderOffset         10.0f
+#define kShuttleBarButtonWidthiPad      77.0f
+#define kShuttleBarButtonWidthiPhone    40.0f
+#define kShuttleBarSliderOffset         5.0f
 
 #define kSYNBundleFullVersion           @"FullVersion"
 #define kSYNBundleBuildTarget           @"BuildTarget"
