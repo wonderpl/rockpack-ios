abfb0034d6c1598da29dbe700ed156715573701d
Kish Patel <kish@kishpatel.com>
2013-05-30 14:19:30 +0100

    Copy Change, 'my channels' 'my subscriptions'

--- RockPack/SYNSubscriptionsViewController.m
+++ RockPack/SYNSubscriptionsViewController.m
@@ -117,9 +117,21 @@
     {
         NSInteger totalChannels = self.user.subscriptions.count;
         
+        if(self.user == appDelegate.currentUser)
+        {
+            [self.headerView setTitle: @"MY SUBSCRIPTIONS"
+                            andNumber: totalChannels];
+        }
+        
+        else
+        {
         [self.headerView setTitle: @"SUBSCRIPTIONS"
                         andNumber: totalChannels];
+        }
+        
     }
+    
+    
     
     [self.channelThumbnailCollectionView reloadData];
 }
