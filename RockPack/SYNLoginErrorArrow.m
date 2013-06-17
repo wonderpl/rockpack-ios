b405c52b7e53890f783b73196913071523b1abbc
Kish Patel <kish@kishpatel.com>
2013-06-17 18:47:14 +0100

    Portrait error fix and some other quick tweaks

--- RockPack/SYNLoginErrorArrow.m
+++ RockPack/SYNLoginErrorArrow.m
@@ -8,6 +8,7 @@
 
 #import "SYNLoginErrorArrow.h"
 #import "UIFont+SYNFont.h"
+#import "SYNDeviceManager.h"
 
 @implementation SYNLoginErrorArrow
 
@@ -15,30 +16,44 @@
 {
     self = [super init];
     if (self) {
-        backgroundImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"LoginError.png"]];
-        [self addSubview:backgroundImageView];
         
-        self.frame = backgroundImageView.frame;
+
         
-        CGRect labelFrame = self.frame;
-        labelFrame.origin.x += 30.0;
-        labelFrame.size.width -= 30.0;
-        labelFrame.origin.y += 5.0;
-        labelFrame.size.height -= 5.0;
-        messageLabel = [[UILabel alloc] initWithFrame:labelFrame];
+        if(UIInterfaceOrientationIsPortrait([[SYNDeviceManager sharedInstance] orientation]))
+        {
+            backgroundImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"LoginErrorPortrait.png"]];
+            [self addSubview:backgroundImageView];
+            self.frame = backgroundImageView.frame;
+            
+            CGRect labelFrame = self.frame;
+            labelFrame.origin.x += 30.0;
+            labelFrame.size.width -= 40.0;
+            labelFrame.origin.y += 4.0;
+            labelFrame.size.height -= 5.0;
+            messageLabel = [[UILabel alloc] initWithFrame:labelFrame];
+        }
+        
+        else
+        {
+            backgroundImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"LoginError.png"]];
+            [self addSubview:backgroundImageView];
+            self.frame = backgroundImageView.frame;
+            
+            CGRect labelFrame = self.frame;
+            labelFrame.origin.x += 30.0;
+            labelFrame.size.width -= 30.0;
+            labelFrame.origin.y += 4.0;
+            labelFrame.size.height -= 5.0;
+            messageLabel = [[UILabel alloc] initWithFrame:labelFrame];
+        }
         
         messageLabel.font = [UIFont rockpackFontOfSize:13.0];
         messageLabel.textColor = [UIColor whiteColor];
         messageLabel.backgroundColor = [UIColor clearColor];
         messageLabel.textAlignment = NSTextAlignmentLeft;
-        
-        
+        messageLabel.numberOfLines = 2;        
         
         [self addSubview:messageLabel];
-        
-        
-        
-        
         
     }
     return self;
