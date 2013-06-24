c81a93c4da18ae66f57bb5ed36e3282660bb9634
Nick Banks <nick.banks@synchromation.com>
2013-06-24 13:39:25 +0100

    Fixed 52208539 (Revert back to Original Cover when Cancelling Edit (and other fields))

--- RockPack/SYNChannelDetailViewController.m
+++ RockPack/SYNChannelDetailViewController.m
@@ -1400,6 +1400,8 @@
         
         [self displayChannelDetails];
         
+        self.currentWebImageOperation = [self loadBackgroundImage];
+        
         [self.videoThumbnailCollectionView reloadData];
     }
 
