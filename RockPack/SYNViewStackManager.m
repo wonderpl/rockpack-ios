43a0e806c50dae984ef69d06743ec6c99edc7810
Michael Michailidis <mmick66@gmail.com>
2013-07-04 15:18:42 +0100

    Added View Stack Manager Class

---
+++ RockPack/SYNViewStackManager.m
@@ -0,0 +1,13 @@
+//
+//  SYNViewStackManager.m
+//  rockpack
+//
+//  Created by Michael Michailidis on 04/07/2013.
+//  Copyright (c) 2013 Nick Banks. All rights reserved.
+//
+
+#import "SYNViewStackManager.h"
+
+@implementation SYNViewStackManager
+
+@end
