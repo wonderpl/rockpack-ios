c642728a0d2b2b0440a218d893651243ee8b0722
Zaid Ramzi <Zaid@Rockpack.com>
2013-07-09 16:49:58 +0100

    tweaks UIAutomation for Jenkins

--- rockpackTests/UIAutomation/buildAndTest.sh
+++ rockpackTests/UIAutomation/buildAndTest.sh
@@ -1,3 +1,5 @@
+osascript ./SimulatorReset.txt
+
 xcodebuild \
 ARCHS=i386 \
 ONLY_ACTIVE_ARCH=NO \
@@ -10,6 +12,6 @@ DSTROOT="/Users/$USER/Documents/UIAutomationBuild/build" \
 TARGETED_DEVICE_FAMILY="2" \
 install
 
-osascript ./SimulatorReset.txt
+./choose_sim_device "iPad"
 
 instruments -t BasicTest "/Users/$USER/Documents/UIAutomationBuild/build/Applications/rockpack.app" -e UIASCRIPT "./BasicTest.js" -e UIARESULTSPATH "./result"
