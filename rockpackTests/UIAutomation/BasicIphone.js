469bdd370bd70a14a41180067edcff44a3471ee2
Zaid Ramzi <Zaid@Rockpack.com>
2013-07-09 16:05:45 +0100

    making automated UI tests more robust by delaying script action when async functions are called

--- rockpackTests/UIAutomation/BasicIphone.js
+++ rockpackTests/UIAutomation/BasicIphone.js
@@ -12,31 +12,50 @@ UIATarget.onAlert = function onAlert(alert) {
     return false; // use default handler
  }
 
+
 target.pushTimeout(10);
+//Login
+target.delay(1);
 target.frontMostApp().mainWindow().buttons()["Login Navigation"].tap();
 target.frontMostApp().keyboard().typeString("automator");
 target.frontMostApp().mainWindow().secureTextFields()["Password Field"].tap();
 target.frontMostApp().keyboard().typeString("banana\n");
+
+//Navigate to a channel
 window.staticTexts()["Page Title"].value() == "CHANNELS";
 window.staticTexts()["Page Title"].withValueForKey(1, "isVisible");
 target.frontMostApp().mainWindow().buttons()["onboarding button ok"].tap();
 target.frontMostApp().mainWindow().scrollViews()[0].staticTexts()["ALL CATEGORIES"].tapWithOptions({tapOffset:{x:0.36, y:0.08}});
+target.delay(2); // Give time for loading category data.
 window.scrollViews()[0].tableViews()["Genre Table"].withValueForKey(1,"isVisible");
-window.scrollViews()[0].tableViews()["Genre Table"].groups()["MUSIC"].withValueForKey(1,"isVisible");
+window.scrollViews()[0].tableViews()["Genre Table"].groups()["MUSIC"].checkIsValid();
 target.frontMostApp().mainWindow().scrollViews()[0].tableViews()["Genre Table"].groups()["MUSIC"].buttons()[0].tap();
-target.frontMostApp().mainWindow().scrollViews()[0].tableViews()["Genre Table"].cells()["POP"].withValueForKey(1,"isVisible");
+target.frontMostApp().mainWindow().scrollViews()[0].tableViews()["Genre Table"].cells()["POP"].checkIsValid();
 target.frontMostApp().mainWindow().scrollViews()[0].tableViews()["Genre Table"].cells()["POP"].tap();
+target.delay(2); //Allow cell data to be loaded (async call)
 target.frontMostApp().mainWindow().scrollViews()[0].collectionViews()[1].cells()[0].tap();
+target.delay(2); //Allow videos to load
 target.frontMostApp().mainWindow().buttons()["onboarding button ok"].tap();
+window.logElementTree();
 target.frontMostApp().mainWindow().buttons()["onboarding button ok"].tap();
+
+//Subscribe
 target.frontMostApp().mainWindow().buttons()["ButtonSubscribe"].tap();
 target.frontMostApp().mainWindow().buttons()["ButtonNavCD"].tap();
+
+//Navigate to profile
 target.frontMostApp().mainWindow().tableViews()["Navigation Table"].cells()["PROFILE"].tap();
 window.staticTexts()["Page Title"].value() == "PROFILE";
 target.frontMostApp().mainWindow().scrollViews()[0].staticTexts()["MY SUBSCRIPTIONS (1)"].tap();
+
+//Select channel
 target.frontMostApp().mainWindow().scrollViews()[0].collectionViews()[2].cells()[0].tap();
+
+//Unsubscribe
 target.frontMostApp().mainWindow().buttons()["ButtonSubscribe"].tap();
 target.frontMostApp().mainWindow().buttons()["ButtonBackCD"].tap();
+
+//Logout
 target.frontMostApp().mainWindow().buttons()["ButtonNav"].tap();
 target.frontMostApp().mainWindow().buttons()["ButtonSettings"].tap();
 target.frontMostApp().mainWindow().tableViews()["Settings Table"].scrollDown();
