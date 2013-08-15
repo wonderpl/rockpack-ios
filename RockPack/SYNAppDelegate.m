5bc6c21c1f7747640db9838e24e3235a7ed18e6f
Michael Michailidis <mmick66@gmail.com>
2013-08-15 21:45:03 +0100

    [Fixes #55255352]

--- RockPack/SYNAppDelegate.m
+++ RockPack/SYNAppDelegate.m
@@ -1424,11 +1424,15 @@
 
 - (void) connectionDidFinishLoading: (NSURLConnection *) connection
 {
-    if (self.rockpackURL)
+    if(self.currentUser.currentValue && self.currentOAuth2Credentials && ![self.currentOAuth2Credentials hasExpired])
     {
-        NSURL *url = [NSURL URLWithString: self.rockpackURL];
-        [self parseAndActionRockpackURL: url];
+        if (self.rockpackURL)
+        {
+            NSURL *url = [NSURL URLWithString: self.rockpackURL];
+            [self parseAndActionRockpackURL: url];
+        }
     }
+    
 }
 
 
