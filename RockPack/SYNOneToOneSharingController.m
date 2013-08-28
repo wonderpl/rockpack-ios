166b149ccf9e3dbb5af89e75844a11371933adf7
Michael Michailidis <mmick66@gmail.com>
2013-08-28 13:48:30 +0100

    Added basic functionality to one to one sharing, untested

--- RockPack/SYNOneToOneSharingController.m
+++ RockPack/SYNOneToOneSharingController.m
@@ -7,32 +7,87 @@
 //
 
 #import "SYNOneToOneSharingController.h"
+#import "UIFont+SYNFont.h"
+#import "Friend.h"
+#import <AddressBook/AddressBook.h>
+#import "SYNAppDelegate.h"
+
+
+#define kOneToOneSharingViewId @"kOneToOneSharingViewId"
 
 @interface SYNOneToOneSharingController ()
+
+@property (nonatomic, strong) IBOutlet UITextView* messageTextView;
+@property (nonatomic, strong) IBOutlet UITextField* searchTextField;
 
 @end
 
 @implementation SYNOneToOneSharingController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    self.messageTextView.font = [UIFont rockpackFontOfSize:self.messageTextView.font.pointSize];
+    
+    self.searchTextField.font = [UIFont rockpackFontOfSize:self.searchTextField.font.pointSize];
+    
+    
+    
+    switch (ABAddressBookGetAuthorizationStatus())
+    {
+        case kABAuthorizationStatusAuthorized:
+            
+            break;
+            
+        case kABAuthorizationStatusDenied:
+            
+            break;
+            
+        case kABAuthorizationStatusNotDetermined:
+            
+            break;
+            
+        case kABAuthorizationStatusRestricted:
+            
+            break;
+    }
+    
+    
 }
 
-- (void)didReceiveMemoryWarning
+-(void)getDataFromAddressBook
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    CFErrorRef error = NULL;
+    ABAddressBookRef addressBookRef = ABAddressBookCreateWithOptions(NULL, &error);
+    
+    NSArray *arrayOfAllPeople = (__bridge_transfer NSArray *)ABAddressBookCopyArrayOfAllPeople(addressBookRef);
+    
+    SYNAppDelegate* appDelegate = (SYNAppDelegate*)[[UIApplication sharedApplication] delegate];
+    NSInteger total = [arrayOfAllPeople count];
+    NSString *firstName, *lastName, *emailAddress;
+    Friend* contactFriend;
+    for (NSUInteger peopleCounter = 0; peopleCounter < total; peopleCounter++)
+    {
+        
+        ABRecordRef currentPerson = (__bridge ABRecordRef)[arrayOfAllPeople objectAtIndex:peopleCounter];
+        
+        
+        
+        firstName = (__bridge_transfer NSString *)ABRecordCopyValue(currentPerson, kABPersonFirstNameProperty);
+        lastName = (__bridge_transfer NSString *)ABRecordCopyValue(currentPerson, kABPersonLastNameProperty);
+        emailAddress = (__bridge_transfer NSString *)ABRecordCopyValue(currentPerson, kABPersonEmailProperty);
+        
+        NSLog(@"Contact found: %@, %@, %@", firstName, lastName, emailAddress);
+        
+        contactFriend = [Friend insertInManagedObjectContext: appDelegate.searchManagedObjectContext];
+        contactFriend.viewId = kOneToOneSharingViewId;
+        
+        
+        
+    }
 }
 
 @end
