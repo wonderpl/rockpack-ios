2cadce5375b648b5c8122a44d65adbe6c189e5bc
Michael Michailidis <mmick66@gmail.com>
2013-08-28 11:26:53 +0100

    Added one to one sharing classes

---
+++ RockPack/SYNOneToOneSharingController.m
@@ -0,0 +1,38 @@
+//
+//  SYNOneToOneSharingController.m
+//  rockpack
+//
+//  Created by Michael Michailidis on 28/08/2013.
+//  Copyright (c) 2013 Nick Banks. All rights reserved.
+//
+
+#import "SYNOneToOneSharingController.h"
+
+@interface SYNOneToOneSharingController ()
+
+@end
+
+@implementation SYNOneToOneSharingController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
