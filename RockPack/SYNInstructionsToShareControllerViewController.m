11dafdc8af6bcaa23dbfc7e22b28b3d96b811e18
Michael Michailidis <mmick66@gmail.com>
2013-09-12 16:45:39 +0100

    Added states in the instructions

--- RockPack/SYNInstructionsToShareControllerViewController.m
+++ RockPack/SYNInstructionsToShareControllerViewController.m
@@ -9,6 +9,15 @@
 #import "SYNInstructionsToShareControllerViewController.h"
 #import "UIFont+SYNFont.h"
 
+typedef enum InstructionsShareState {
+
+    InstructionsShareStateInit = 0,
+    InstructionsShareStatePressAndHold,
+    InstructionsShareStateChooseAction,
+    InstructionsShareStateGoodJob,
+    InstructionsShareStateShared
+
+} InstructionsShareState;
 @interface SYNInstructionsToShareControllerViewController ()
 
 @property (strong, nonatomic) IBOutlet UILabel *instructionsLabel;
@@ -16,7 +25,7 @@
 @property (strong, nonatomic) IBOutlet UIButton *okButton;
 @property (strong, nonatomic) IBOutlet UIImageView *videoImageView;
 
-
+@property (nonatomic) InstructionsShareState state;
 @end
 
 @implementation SYNInstructionsToShareControllerViewController
@@ -37,6 +46,38 @@
 -(IBAction)okayButtonPressed:(id)sender
 {
     
+}
+
+-(void)setState:(InstructionsShareState)state
+{
+    switch (state)
+    {
+        case InstructionsShareStateInit:
+            self.instructionsLabel.text = NSLocalizedString(@"instruction_initial", nil);
+            self.subLabel.text = NSLocalizedString(@"instruction_initial_subtext", nil);
+            self.subLabel.hidden = NO;
+            break;
+            
+        case InstructionsShareStatePressAndHold:
+            self.instructionsLabel.text = NSLocalizedString(@"instruction_press_hold", nil);
+            self.subLabel.text = @"";
+            self.subLabel.hidden = YES;
+            break;
+            
+        case InstructionsShareStateChooseAction:
+            self.instructionsLabel.text = NSLocalizedString(@"instruction_choose_action", nil);
+            break;
+            
+        case InstructionsShareStateGoodJob:
+            self.instructionsLabel.text = NSLocalizedString(@"instruction_good_job", nil);
+            break;
+            
+        case InstructionsShareStateShared:
+            self.instructionsLabel.text = NSLocalizedString(@"channels_screen_loading_categories", nil);
+            break;
+            
+            
+    }
 }
 
 
