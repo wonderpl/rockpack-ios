a200bbee9688e6102f0c9d78755347562222867f
Michael Michailidis <mmick66@gmail.com>
2013-09-12 16:27:15 +0100

    Added new on baording classes

---
+++ RockPack/SYNInstructionsToShareControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  SYNInstructionsToShareControllerViewController.h
+//  rockpack
+//
+//  Created by Michael Michailidis on 12/09/2013.
+//  Copyright (c) 2013 Nick Banks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYNInstructionsToShareControllerViewController : UIViewController
+
+@end
